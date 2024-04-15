d7e8dc50c47359991744a12bab3d222eb7768358
ROUSSIN Benjamin <benroussin@hotmail.com>
2024-04-15 11:30:51 -0300

    Better Toast on no detail

--- LinkBlazor.DialogAndToast/Services/DialogService.cs
+++ LinkBlazor.DialogAndToast/Services/DialogService.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LinkBlazor
 {
